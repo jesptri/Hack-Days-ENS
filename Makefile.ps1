fcb70d3acb277b5597c40342e81c5a97d0318b21
jesptri <julespinoux@gmail.com>
2025-06-07 15:31:01 +0200

    FIX - clean Makefile.ps1

--- Makefile.ps1
+++ Makefile.ps1
@@ -1,30 +1,4 @@
-# /!\ /!\ /!\ /!\ /!\ /!\ /!\ DISCLAIMER /!\ /!\ /!\ /!\ /!\ /!\ /!\ /!\
-#
-# This Makefile is only meant to be used for DEVELOPMENT purpose as we are
-# changing the user id that will run in the container.
-#
-# PLEASE DO NOT USE IT FOR YOUR CI/PRODUCTION/WHATEVER...
-#
-# /!\ /!\ /!\ /!\ /!\ /!\ /!\ /!\ /!\ /!\ /!\ /!\ /!\ /!\ /!\ /!\ /!\ /!\
-#
-# Note to developers:
-#
-# While editing this file, please respect the following statements:
-#
-# 1. Every variable should be defined in the ad hoc VARIABLES section with a
-#    relevant subsection
-# 2. Every new rule should be defined in the ad hoc RULES section with a
-#    relevant subsection depending on the targeted service
-# 3. Rules should be sorted alphabetically within their section
-# 4. When a rule has multiple dependencies, you should:
-#    - duplicate the rule name to add the help string (if required)
-#    - write one dependency per line to increase readability and diffs
-# 5. .PHONY rule statement should be written after the corresponding rule
-# ==============================================================================
-# VARIABLES
-
-# ==============================================================================
-
+# Description: PowerShell script to manage development tasks for a Docker project with Django and Node.js.
 <#
     .SYNOPSIS
     PowerShell script to manage development tasks for a Docker project with Django and Node.js.
@@ -60,37 +34,6 @@
     .PARAMETER Help
     Display the help message and available commands in the script.
 #>
-
-# $BOLD := \033[1m
-# $RESET := \033[0m
-# $GREEN := \033[1;32m
-
-# -- Database
-# $DB_HOST            = postgresql
-# $DB_PORT            = 5432
-
-# # -- Docker
-# # Get the current user ID to use for docker run and docker exec commands
-# $DOCKER_UID          = $(shell id -u)
-# $DOCKER_GID          = $(shell id -g)
-# $DOCKER_USER         = $(DOCKER_UID):$(DOCKER_GID)
-# $COMPOSE             = DOCKER_USER=$(DOCKER_USER) docker compose
-# $COMPOSE_EXEC        = $(COMPOSE) exec
-# $COMPOSE_EXEC_APP    = $(COMPOSE_EXEC) app-dev
-# $COMPOSE_RUN         = $(COMPOSE) run --rm
-# $COMPOSE_RUN_APP     = $(COMPOSE_RUN) app-dev
-# $COMPOSE_RUN_CROWDIN = $(COMPOSE_RUN) crowdin crowdin
-
-# # -- Backend
-# $MANAGE              = $(COMPOSE_RUN_APP) python manage.py
-# $MAIL_YARN           = $(COMPOSE_RUN) -w /app/src/mail node yarn
-
-# # -- Frontend
-# $PATH_FRONT          = ./src/frontend
-# $PATH_FRONT_IMPRESS  = $(PATH_FRONT)/apps/impress
-
-# ==============================================================================
-# RULES
 
 # Makefile.ps1 - PowerShell script to manage development tasks
 
@@ -317,7 +260,6 @@ $nodeUrl = "https://nodejs.org/dist/v20.11.1/node-v20.11.1-x64.msi"
 $installerPath = "$env:TEMP\node-lts.msi"
 $yarnDependenciesPath = "./src/frontend/apps/impress"
 
-# TODO fix the installation of Node.js : the command is not detected in the path for execution within the script
 function Install-NodeJS {
     param (
         [string]$url,
